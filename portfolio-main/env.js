7195da1c9b88f223535b2b46dc34116858e53417
Magi Sharma J <181385201+magi8101@users.noreply.github.com>
2025-01-15 22:27:08 +0530

    SECURITY

--- portfolio-main/env.js
+++ portfolio-main/env.js
@@ -1,6 +1,6 @@
 window.env = {
-    EMAILJS_PUBLIC_KEY: 'b7N7TkBN7Uxqocng6',
-    EMAILJS_SERVICE_ID: 'service_g2340ij',
-    EMAILJS_TEMPLATE_ID_MESSAGE: 'template_cvvfre6',
-    EMAILJS_TEMPLATE_ID_THANKYOU: 'template_34pokrt'
+    EMAILJS_PUBLIC_KEY: '*************',
+    EMAILJS_SERVICE_ID: '**************',
+    EMAILJS_TEMPLATE_ID_MESSAGE: '******************',
+    EMAILJS_TEMPLATE_ID_THANKYOU: '******************'
 };
